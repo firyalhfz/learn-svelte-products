8833e4f8268c920b4cbc7fbe3becbd4868dff59a
firyalhfz <iyaalfiryal@gmail.com>
2024-07-23 14:59:04 +0700

    feat: load Data

---
+++ src/routes/products/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Product from './product.svelte';
+
+  export let data;
+  export let products = data.products;
+  // export let Component = data.Component;
+</script>
+
+<h1>{data.title}</h1>
+{#each products as product}
+  <div>
+    <Product {product} />
+  </div>
+{/each}
---
+++ src/routes/products/[productId]/+page.server.js
@@ -0,0 +1,21 @@
+import {
+  // error,
+  redirect
+} from '@sveltejs/kit';
+
+export const load = async (serverLoadEvent) => {
+  const { fetch, params, url, route } = serverLoadEvent;
+  console.log({ params, url, route: route.id });
+  const { productId } = params;
+  if (productId > 3) {
+    // throw error(404, { message: 'Product not found', hint: 'Try a different product' });
+    throw redirect(307, '/products');
+  }
+  const title = 'Product Details';
+  const response = await fetch(`http://localhost:4000/products/${productId}`);
+  const product = await response.json();
+  return {
+    title,
+    product
+  };
+};
