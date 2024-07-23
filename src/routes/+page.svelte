4d45df9c9984877f3caa664e2292a686ba8db335
firyalhfz <iyaalfiryal@gmail.com>
2024-07-23 20:07:54 +0700

    feat: link option

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,27 @@
+<script>
+  import { goto, preloadData } from '$app/navigation';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<a href="/products">Products</a>
+<a href="/products" data-sveltekit-reload>Products</a>
+
+<button
+  on:mouseover={async () => {
+    await preloadData('/products');
+  }}
+  on:focus={async () => {
+    await preloadData('/products');
+  }}
+  on:click={() => {
+    goto('/products');
+  }}
+>
+  Go to Producst</button
+>
+
+<style>
+  .tall {
+    height: 100vh;
+  }
+</style>
