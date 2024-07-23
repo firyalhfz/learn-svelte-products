1cd17fd048b81df95ed27954d4909a78f236fc22
firyalhfz <iyaalfiryal@gmail.com>
2024-07-23 16:18:02 +0700

    feat: parents data

--- src/routes/products/+page.js
+++ src/routes/products/+page.js
@@ -1,14 +1,14 @@
-// import Product from './product.svelte';
+import Product from './product.svelte';
 
-// export const load = async (loadEvent) => {
-//   console.log('Load function called in page.js');
-//   const { fetch } = loadEvent;
-//   const title = 'List of available products';
-//   const response = await fetch('http://localhost:4000/products');
-//   const products = await response.json();
-//   return {
-//     title,
-//     products,
-//     Component: Product
-//   };
-// };
+export const load = async (loadEvent) => {
+  console.log('Load function called in page.js');
+  const { fetch } = loadEvent;
+  const title = 'List of available products';
+  const response = await fetch('http://localhost:4000/products');
+  const products = await response.json();
+  return {
+    title,
+    products,
+    Component: Product
+  };
+};
--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -3,12 +3,12 @@
 
   export let data;
   export let products = data.products;
-  // export let Component = data.Component;
+  export let Component = data.Component;
 </script>
 
 <h1>{data.title}</h1>
 {#each products as product}
   <div>
-    <Product {product} />
+    <Component {product} />
   </div>
 {/each}
